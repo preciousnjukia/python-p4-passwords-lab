642efe18706a5cf2069b509e308c685bbbed355d
preciousnjukia <preciousnjukia@gmail.com>
2023-12-10 10:58:06 +0300

    Assignment done

--- server/app.py
+++ server/app.py
@@ -2,9 +2,12 @@
 
 from flask import request, session
 from flask_restful import Resource
+from flask_bcrypt import Bcrypt
 
 from config import app, db, api
 from models import User
+
+bcrypt = Bcrypt(app)
 
 class ClearSession(Resource):
 
@@ -16,28 +19,60 @@ class ClearSession(Resource):
         return {}, 204
 
 class Signup(Resource):
-    
     def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
+        json_data = request.get_json()
+        username = json_data.get('username')
+        password = json_data.get('password')
+
+        if not username or not password:
+            return {'message': 'Both username and password are required'}, 400
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return {'message': 'Username already taken'}, 400
+
+        user = User(username=username, password_hash=password)
         db.session.add(user)
         db.session.commit()
+
+        session['user_id'] = user.id
+
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict()
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        json_data = request.get_json()
+        username = json_data.get('username')
+        password = json_data.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict()
+        else:
+            return {'message': 'Invalid credentials'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.clear()
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
